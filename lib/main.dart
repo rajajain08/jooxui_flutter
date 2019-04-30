f59116ead86a4b5c5d8010556dfd4467c03ef4c3
Raja Jain <rajajain08@gmail.com>
2019-04-30 23:38:46 +0530

    added second screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:jooxui/data.dart';
+import 'package:jooxui/screens/music_onboadring_third.dart';
+import 'package:jooxui/screens/music_onboarding_second.dart';
 import 'components/noscroll_side_effect.dart';
 import 'package:flare_flutter/flare_actor.dart';
+// import 'package:page_transition/page_transition.dart';
+// import 'package:swipedetector/swipedetector.dart';
 
-void main() => runApp(MaterialApp(home: MyApp()));
+void main() => runApp(MaterialApp(home: Home()));
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  PageController controller;
+  var pages = [
+    MyApp(),
+    MusicOnboardingSecond(),
+    MusicOnboardingThird(),
+  ];
+  @override
+  void initState() {
+    controller = new PageController(
+      initialPage: 0,
+      keepPage: false,
+      // viewportFraction: 0.8,
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+        scrollDirection: Axis.vertical,
+        onPageChanged: (value) {
+          setState(() {});
+        },
+        controller: controller,
+        itemCount: pages.length,
+        itemBuilder: (context, index) => pages[index]);
+  }
+}
 
 class MyApp extends StatefulWidget {
   // This widget is the root of your application.
@@ -16,6 +56,7 @@ class _MyAppState extends State<MyApp> {
   ScrollController controller1 = new ScrollController();
   ScrollController controller2 = new ScrollController();
   ScrollController controller3 = new ScrollController();
+
   @override
   void initState() {
     super.initState();
@@ -201,27 +242,86 @@ class _MyAppState extends State<MyApp> {
                 ),
               ),
             ),
-            Container(
-              height: 30,
-              width: 30,
-              child: FlareActor("assets/flare_animation/arrow.flr",
+            Hero(
+              tag: "lol",
+              child: Container(
+                color: Colors.transparent,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(children: <Widget>[
+                      Container(
+                        height: 30,
+                        width: 30,
+                        child: FlareActor("assets/flare_animation/arrow.flr",
 
-                  //  alignment: Alignment(bulletXPoint, stream.data),
-                  fit: BoxFit.fitWidth,
-                  animation: "Untitled"),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Center(
-                child: Text(
-                  "Swipe Up",
-                  style: TextStyle(color: Colors.white, fontSize: 16),
+                            //  alignment: Alignment(bulletXPoint, stream.data),
+                            fit: BoxFit.fitWidth,
+                            animation: "Untitled"),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Center(
+                          child: Text(
+                            "Swipe Up",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ],
                 ),
               ),
             ),
+
+            // SwipeDetector(
+            //   swipeConfiguration: SwipeConfiguration(
+            //       verticalSwipeMinVelocity: 30.0,
+            //       verticalSwipeMinDisplacement: 20.0,
+            //       verticalSwipeMaxWidthThreshold: 100.0,
+            //       horizontalSwipeMaxHeightThreshold: 50.0,
+            //       horizontalSwipeMinDisplacement: 50.0,
+            //       horizontalSwipeMinVelocity: 200.0),
+            //   child: Container(
+            //     color: Colors.transparent,
+            //     child: Row(
+            //       mainAxisSize: MainAxisSize.max,
+            //       mainAxisAlignment: MainAxisAlignment.center,
+            //       children: <Widget>[
+            //         Column(children: <Widget>[
+
+            //         ]),
+            //       ],
+            //     ),
+            //   ),
+            //   // onSwipeUp: () {
+            //   //   Navigator.push(
+            //   //       context,
+            //   //       PageRouteBuilder(
+            //   //           opaque: false,
+            //   //           pageBuilder: (BuildContext context, _, __) {
+            //   //             return MusicOnboardingSecond();
+            //   //           },
+            //   //           transitionsBuilder: (___, Animation<double> animation,
+            //   //               ____, Widget child) {
+            //   //             return FadeTransition(
+            //   //               opacity: animation,
+            //   //               child: SlideTransition(
+            //   //                 position: Tween<Offset>(
+            //   //                         begin: Offset(0.0, 0.9),
+            //   //                         end: Offset(0.0, 0.0))
+            //   //                     .animate(animation),
+            //   //                 child: child,
+            //   //               ),
+            //   //             );
+            //   //           }));
+            //   // },
+            // )
           ],
         ),
       ),
     );
   }
 }
+// PageTransition asd;
---
+++ lib/screens/music_onboarding_second.dart
@@ -0,0 +1,267 @@
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+class MusicOnboardingSecond extends StatefulWidget {
+  MusicOnboardingSecond({Key key}) : super(key: key);
+
+  _MusicOnboardingSecondState createState() => _MusicOnboardingSecondState();
+}
+
+class _MusicOnboardingSecondState extends State<MusicOnboardingSecond> {
+  PageController controller;
+  var pages = [
+    Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Stack(overflow: Overflow.visible, children: <Widget>[
+        Column(children: <Widget>[
+          Container(
+            height: 450,
+            width: 300,
+            alignment: Alignment.bottomCenter,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              image: DecorationImage(
+                fit: BoxFit.fitHeight,
+                image: NetworkImage(
+                    'https://psmag.com/.image/t_share/MTMzMDU5MDQzMjY1ODgyNzU1/taylor-swift-2jpg.jpg'),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Taylor Didi",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 22),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16.0),
+            child: FlatButton(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color.fromRGBO(59, 219, 247, 1)),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                height: 50,
+                width: 180,
+                child: Center(
+                  child: Text(
+                    "Follow",
+                    style: TextStyle(
+                      color: Color.fromRGBO(59, 219, 247, 1),
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              onPressed: () {},
+            ),
+          )
+        ]),
+        Positioned(
+          top: -20,
+          left: -20,
+          child: Image.asset(
+            'assets/images/blue_tick.png',
+            height: 120,
+            width: 120,
+          ),
+        ),
+      ]),
+    ),
+    Column(
+      children: <Widget>[
+        Container(
+          height: 450,
+          width: 300,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: DecorationImage(
+              fit: BoxFit.fitHeight,
+              image: NetworkImage(
+                  'https://cdn.baeblemusic.com/images/bblog/3-21-2017/ed_sheeranblog-580.jpg'),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: FlatButton(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Color.fromRGBO(59, 219, 247, 1)),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              height: 50,
+              width: 180,
+              child: Center(
+                child: Text(
+                  "Follow",
+                  style: TextStyle(
+                    color: Color.fromRGBO(59, 219, 247, 1),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            onPressed: () {},
+          ),
+        )
+      ],
+    ),
+    Column(
+      children: <Widget>[
+        Container(
+          height: 450,
+          width: 300,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: DecorationImage(
+              fit: BoxFit.fitHeight,
+              image: NetworkImage(
+                  'https://akns-images.eonline.com/eol_images/Entire_Site/2018827/rs_1024x759-180927184903-1024-ariana-grande1.jpg?fit=inside%7C900:auto&output-quality=90'),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: FlatButton(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Color.fromRGBO(59, 219, 247, 1)),
+                borderRadius: BorderRadius.circular(50),
+              ),
+              height: 50,
+              width: 180,
+              child: Center(
+                child: Text(
+                  "Follow",
+                  style: TextStyle(
+                    color: Color.fromRGBO(59, 219, 247, 1),
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            onPressed: () {},
+          ),
+        )
+      ],
+    ),
+  ];
+  @override
+  void initState() {
+    controller = new PageController(
+      initialPage: 0,
+      keepPage: false,
+      viewportFraction: 0.8,
+    );
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          // stops: [0.1, 0.5, 0.7, 0.9],
+          colors: [
+            Color.fromRGBO(112, 124, 146, 1),
+            Color.fromRGBO(32, 30, 35, 1),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
+          title: Container(
+            height: 35,
+            width: 180,
+            decoration: BoxDecoration(
+                color: Color.fromRGBO(32, 30, 35, 1),
+                borderRadius: BorderRadius.circular(16)),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.terrain,
+                      color: Color.fromRGBO(59, 219, 247, 1),
+                      size: 30,
+                    ),
+                    Text("500")
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.data_usage,
+                      color: Color.fromRGBO(59, 219, 247, 1),
+                      size: 30,
+                    ),
+                    Text("500")
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Text(
+                "Artists we recommend",
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              height: 550,
+              width: double.infinity,
+              child: PageView.builder(
+                  scrollDirection: Axis.horizontal,
+                  onPageChanged: (value) {
+                    setState(() {});
+                  },
+                  controller: controller,
+                  itemCount: pages.length,
+                  itemBuilder: (context, index) => Center(child: pages[index])),
+            ),
+            Column(children: <Widget>[
+              Container(
+                height: 30,
+                width: 30,
+                child: FlareActor("assets/flare_animation/arrow.flr",
+
+                    //  alignment: Alignment(bulletXPoint, stream.data),
+                    fit: BoxFit.fitWidth,
+                    animation: "Untitled"),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Text(
+                    "Swipe Up",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
